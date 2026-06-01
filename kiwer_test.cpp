70a9558f68f7ed63a72fb55696daaa7f0e123b0f
Final-JK <finaljk@gmail.com>
2026-06-01 11:29:29 +0900

    [test] divide unit test case by Kiwer/Nemo/AutoTradingSystem
    Signed-off-by: Final-JK <finaljk@gmail.com>

---
+++ kiwer_test.cpp
@@ -0,0 +1,25 @@
+﻿#include "gmock/gmock.h"
+#include "kiwer_api.cpp"
+
+//=======================
+// 키워 드라이버 test항목
+
+TEST(TradingSystemKiwerDriver, loginTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+
+TEST(TradingSystemKiwerDriver, buyTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+
+TEST(TradingSystemKiwerDriver, sellTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+
+TEST(TradingSystemKiwerDriver, getPriceTest) {
+    // 로그인이 되는지 체크한다.
+
+}
