16f8e85af8761da18a9aa5aaea20bd64bfa4fac2
HEEJAE,KIM <kim60212@naver.com>
2026-06-01 16:06:35 +0900

    [Nemo_Adapter] Implement methods
    Signed-off-by: HEEJAE,KIM <kim60212@naver.com>

--- nemo_api_adapter.cpp
+++ nemo_api_adapter.cpp
@@ -5,16 +5,16 @@
 class NemoAdapter : public IStockerBrockerDriver {
 public:
 	void login(string id, string password) override {
-
+		nemoApi.certification(id, password);
 	};
 	void buy(string stock_code, int count, int price) override {
-
+		nemoApi.purchasingStock(stock_code, price, count);
 	};
 	void sell(string stock_code, int count, int price) override {
-
+		nemoApi.sellingStock(stock_code, price, count);
 	};
 	int getPrice(string stock_code, int minute = 0) override {
-
+		return nemoApi.getMarketPrice(stock_code, minute);
 	};
 private:
 	NemoAPI nemoApi;
