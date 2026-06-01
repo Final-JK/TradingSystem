339baa850d56645ab18f29296916c8fe0b1d85a1
Final-JK <finaljk@gmail.com>
2026-06-01 10:58:18 +0900

    [test] add unit test case based on ISSUE#1
    Signed-off-by: Final-JK <finaljk@gmail.com>

--- main_test.cpp
+++ main_test.cpp
@@ -2,6 +2,58 @@
 #include "nemo_api.cpp"
 #include "kiwer_api.cpp"
 
+//=======================
+// 키위 드라이버 test항목
+
+TEST(TradingSystemKiwiDriver, loginTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+TEST(TradingSystemKiwiDriver, buyTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+TEST(TradingSystemKiwiDriver, sellTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+TEST(TradingSystemKiwiDriver, getPriceTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+//=======================
+// Nemo Driver
+TEST(TradingSystemNemoDriver, loginTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+TEST(TradingSystemNemoDriver, buyTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+TEST(TradingSystemNemoDriver, sellTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+TEST(TradingSystemNemoDriver, getPriceTest) {
+    // 로그인이 되는지 체크한다.
+
+}
+
+//=======================
+// Auto trading system
+
+TEST(AutoTradinngSystem, mockingBuyNiceTimining) {
+
+}
+
+TEST(AutoTradinngSystem, mockingSellNiceTimining) {
+
+}
+
+
+//=======================
+// Test 분류
 
 TEST(TradingSystem, StartProject) {
     EXPECT_EQ(1, 1);
