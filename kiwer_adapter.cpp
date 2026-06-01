aac64f456b6905d4112e7963c430aeb3056571ff
zentornoe <andycityfc@gmail.com>
2026-06-01 14:43:33 +0900

    [feat] Implement KiwerAdapter in kiwer_adapter.cpp
    Implement KiwerAdapter class that inherits IStockerBrockerDriver interface  and calls KiwerAPI.

--- kiwer_adapter.cpp
+++ kiwer_adapter.cpp
@@ -4,19 +4,19 @@
 class KiwerAdapter : public IStockerBrockerDriver {
 public:
 	void login(string id, string password) override {
-
+		kiwer_api.login(id, password);
 	}
 
 	void buy(string stock_code, int count, int price) override {
-
+		kiwer_api.buy(stock_code, count, price);
 	}
 
 	void sell(string stock_code, int count, int price) override {
-
+		kiwer_api.sell(stock_code, count, price);
 	}
 
 	int getPrice(string stock_code, int minute = 0) override {
-
+		return kiwer_api.currentPrice(stock_code);
 	}
 private:
 	KiwerAPI kiwer_api;
