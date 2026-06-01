ad078f6fd8ed2ee6de025dabe60265c399c57909
Final-JK <finaljk@gmail.com>
2026-06-01 10:31:43 +0900

    기본 commit.
    Unit test Case 는 main_test.cpp 에 추가 하는 형식으로 할 예정임.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
 ﻿#include "gmock/gmock.h"
-#include "nemo_api.cpp"
-#include "kiwer_api.cpp"
-
-
-TEST(TradingSystem, StartProject) {
-    EXPECT_EQ(1, 1);
-}
 
 int main()
 {
---
+++ main_test.cpp
@@ -0,0 +1,8 @@
+﻿#include "gmock/gmock.h"
+#include "nemo_api.cpp"
+#include "kiwer_api.cpp"
+
+
+TEST(TradingSystem, StartProject) {
+    EXPECT_EQ(1, 1);
+}
