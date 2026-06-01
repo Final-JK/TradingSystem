52ab7378f25c3d7f566e68aaf2e80dc093844ca3
Final-JK <finaljk@gmail.com>
2026-06-01 09:41:25 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include "gmock/gmock.h"
+#include "nemo_api.cpp"
+#include "kiwer_api.cpp"
+
+
+TEST(TradingSystem, StartProject) {
+    EXPECT_EQ(1, 1);
+}
+
+int main()
+{
+    testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
+}
+
