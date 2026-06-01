06dca861ef007afd963738aea3bae286af3460e4
Final-JK <finaljk@gmail.com>
2026-06-01 15:14:22 +0900

    [test] Nemo Driver's Test Case implements
    Signed-off-by: Final-JK <finaljk@gmail.com>

--- nemo_test.cpp
+++ nemo_test.cpp
@@ -1,21 +1,130 @@
-﻿#include "gmock/gmock.h"
-#include "nemo_api.cpp"
+#include "gmock/gmock.h"
+#include "nemo_api_adapter.cpp"
+
+#define USING_MOCK 1
+
+#if (1 == USING_MOCK)
+#include "mock_driver.cpp"
+using namespace testing;
+#endif
 
 //=======================
-// Nemo Driver
+// 네모 드라이버 test항목
+
 TEST(TradingSystemNemoDriver, loginTest) {
-    // 로그인이 되는지 체크한다.
+    std::string expect = "[NEMO] A-team login GOOD\n";
+    std::string ID = "A-team";
+    std::string PASSWD = "1234";
+
+
+    std::ostringstream oss; // 임시 buffer생성
+    auto oldCoutStreamBuf = std::cout.rdbuf(); // 기존 cout의 출력 buffer 임시 저장
+    std::cout.rdbuf(oss.rdbuf());  // cout의 출력을 임시 buffer로 바꿔놓기
+
+#if (1 == USING_MOCK)
+    NiceMock<MockDriver> mockDrv;
+
+    EXPECT_CALL(mockDrv, login(_, _))
+        .Times(1);
+    oss << expect;
+
+    mockDrv.login(ID, PASSWD);
+#else
+    IStockerBrockerDriver* nemoDrv = new NemoAdapter();
+    nemoDrv->login(ID, PASSWD);
+#endif
+
+    std::cout.rdbuf(oldCoutStreamBuf); // 기존 cout의 출력 buffer되돌려 놓기
+
+    EXPECT_THAT(oss.str(), Eq(expect));
 
 }
+
 TEST(TradingSystemNemoDriver, buyTest) {
-    // 로그인이 되는지 체크한다.
+    // buy 가 되는지 체크한다.
+    std::string expect = "[Nemo] A-team : buy stock ( price : 1234 ) * ( count : 100000)\n";
+    std::string STOCKCODE = "A-team";
+    int PRICE = 1234;
+    int COUNT = 100000;
+
+
+    std::ostringstream oss; // 임시 buffer생성
+    auto oldCoutStreamBuf = std::cout.rdbuf(); // 기존 cout의 출력 buffer 임시 저장
+    std::cout.rdbuf(oss.rdbuf());  // cout의 출력을 임시 buffer로 바꿔놓기
+
+#if (1 == USING_MOCK)
+    NiceMock<MockDriver> mockDrv;
+
+    EXPECT_CALL(mockDrv, buy(_, _, _))
+        .Times(1);
+    oss << expect;
+
+    mockDrv.buy(STOCKCODE, PRICE, COUNT);
+#else
+    IStockerBrockerDriver* nemoDrv = new NemoAdapter();
+    nemoDrv->buy(STOCKCODE, PRICE, COUNT);
+#endif
+
+    std::cout.rdbuf(oldCoutStreamBuf); // 기존 cout의 출력 buffer되돌려 놓기
+
+    EXPECT_THAT(oss.str(), Eq(expect));
 
 }
+
 TEST(TradingSystemNemoDriver, sellTest) {
-    // 로그인이 되는지 체크한다.
+    // sell 이 되는지 체크한다.
+    std::string expect = "[NEMO] A-team : sell stock ( price : 1234 ) * ( count : 100000)\n";
+    std::string STOCKCODE = "A-team";
+    int PRICE = 1234;
+    int COUNT = 100000;
 
+
+    std::ostringstream oss; // 임시 buffer생성
+    auto oldCoutStreamBuf = std::cout.rdbuf(); // 기존 cout의 출력 buffer 임시 저장
+    std::cout.rdbuf(oss.rdbuf());  // cout의 출력을 임시 buffer로 바꿔놓기
+
+#if (1 == USING_MOCK)
+    NiceMock<MockDriver> mockDrv;
+
+    EXPECT_CALL(mockDrv, sell(_, _, _))
+        .Times(1);
+    oss << expect;
+
+    mockDrv.sell(STOCKCODE, PRICE, COUNT);
+#else
+    IStockerBrockerDriver* nemoDrv = new NemoAdapter();
+    nemoDrv->sell(STOCKCODE, PRICE, COUNT);
+#endif
+
+    std::cout.rdbuf(oldCoutStreamBuf); // 기존 cout의 출력 buffer되돌려 놓기
+
+    EXPECT_THAT(oss.str(), Eq(expect));
 }
-TEST(TradingSystemNemoDriver, getPriceTest) {
-    // 로그인이 되는지 체크한다.
 
+TEST(TradingSystemNemoDriver, getPriceTest) {
+    // getPrice 가 되는지 체크한다.
+    std::string STOCKCODE = "A-team";
+    int expect = 100;
+    int returnValue = -1;
+
+    std::ostringstream oss; // 임시 buffer생성
+    auto oldCoutStreamBuf = std::cout.rdbuf(); // 기존 cout의 출력 buffer 임시 저장
+    std::cout.rdbuf(oss.rdbuf());  // cout의 출력을 임시 buffer로 바꿔놓기
+
+#if (1 == USING_MOCK)
+    NiceMock<MockDriver> mockDrv;
+
+    EXPECT_CALL(mockDrv, getPrice(_, _))
+        .Times(1)
+        .WillRepeatedly(Return(100));
+
+    returnValue = mockDrv.getPrice(STOCKCODE, 1);
+#else
+    IStockerBrockerDriver* nemoDrv = new NemoAdapter();
+    returnValue = nemoDrv->getPrice(STOCKCODE);
+#endif
+
+    std::cout.rdbuf(oldCoutStreamBuf); // 기존 cout의 출력 buffer되돌려 놓기
+
+    EXPECT_THAT(returnValue, Eq(expect));
 }
