e5986f8612790adbd954e62124e16e6386a69e67
HEEJAE,KIM <kim60212@naver.com>
2026-06-01 13:47:18 +0900

    [Nemo_Adapter] Add nemo_api_adapter file
    Signed-off-by: HEEJAE,KIM <kim60212@naver.com>

---
+++ nemo_api_adapter.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "inteface.cpp"
+#include "nemo_api.cpp"
+
+class NemoAdapter : public IStockerBrockerDriver {
+public:
+	void login(string id, string password) override {
+
+	};
+	void buy(string stock_code, int count, int price) override {
+
+	};
+	void sell(string stock_code, int count, int price) override {
+
+	};
+	int getPrice(string stock_code, int minute = 0) override {
+
+	};
+private:
+	NemoAPI nemoApi;
+};
